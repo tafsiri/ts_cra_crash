987b5b57c64074caf4e469e0b35a19ca27631d40
Tom Ketola <tom2017@lunaticlabs.com>
2020-06-12 11:49:40 +0200

    - Initial commit just loading tensorflow/bodypix, which crashes if you run a production build

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as bodyPix from '@tensorflow-models/body-pix';
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
+import * as tf from '@tensorflow/tfjs-core';
+
+async function loadBodyPix()
+{
+  console.log("Starting load");
+  bodyPix.load({
+    architecture: 'MobileNetV1',
+    outputStride: 16,
+    multiplier: 0.5,
+    quantBytes: 1
+  }).then((bodypix) => {
+    console.log("TF load complete, setting state");
+    //this.setState({tensorFlow: bodypix, processVideo: true});
+  });
+}
+
 function App() {
+  loadBodyPix();
   return (
     <div className="App">
       <header className="App-header">
